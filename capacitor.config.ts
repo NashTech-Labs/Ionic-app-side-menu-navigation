ea07bdd3dcad5addd0b4d4be6f14b06b1e093379
Piyush Agrawal <piyush.agrawal@nashtechglobal.com>
2023-12-15 15:41:54 +0530

    ionic side menu nav added

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic-app-side-menu-navigation',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
